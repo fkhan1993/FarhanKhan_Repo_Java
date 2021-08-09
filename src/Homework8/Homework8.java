f2a57576ba180bd8a711da9a6f93976c986de526
fkhan <farhankhn330@gmail.com>
2021-08-09 00:28:36 -0400

    Homework9

--- src/Homework8/Homework8.java
+++ src/Homework8/Homework8.java
@@ -3,121 +3,107 @@ package Homework8;
 import java.lang.String;
 
 public class Homework8 {
-    // Due : Aug 01, 2021
-    //String message = "Good Day";
-    public static void main(String[] args) {
-        String s = toTitleCase("happy new year TO You DeaR");
-        System.out.println(s);
-        System.out.println(reverseString("Happy Holidays"));
-        int[] array = {7, 10, 3, 6, 10};
-        System.out.println(average(array));
-        int arrays[] = {33, 54, 67, 19, 9, 52};
-        taskword("dad");
-        System.out.println("Maximum Value is: " + getMax(arrays));
+ public static void main(String[] args) {
 
     }
+
+    // Due : Aug 01, 2021
 
     /**
      * Write a method to convert given string into Titlecase
-     * <p>
+     *
      * Input sentence = "happy new year TO You DeaR"
      * Final answer = "Happy New Year To You Dear"
      */
-    public static String toTitleCase(String threeWordSentence_1) {
+public static String toTitleCase(String userStr) {
+        String formattedStr = "";
+        String[] userStrArray = userStr.toUpperCase().split(" ");
+        for (String word : userStrArray) {
+        formattedStr = formattedStr + word.charAt(0) + word.substring(1).toLowerCase() + " ";
+        // formattedStr+=word.charAt(0)+word.substring(1).toLowerCase()+" ";
+        }
+        return formattedStr.trim();
+        }
 
-        System.out.println("Before code : " + threeWordSentence_1);
-        // code
-        threeWordSentence_1 = threeWordSentence_1.toLowerCase();
-        for (int i = 0; i <= threeWordSentence_1.length() - 1; i++) {
-            String[] arr1 = threeWordSentence_1.split(" ");
-            arr1[0] = arr1[0].substring(0, 1).toUpperCase() + arr1[0].substring(1);
-            arr1[1] = arr1[1].substring(0, 1).toUpperCase() + arr1[1].substring(1);
-            arr1[2] = arr1[2].substring(0, 1).toUpperCase() + arr1[2].substring(1);
-            arr1[3] = arr1[3].substring(0, 1).toUpperCase() + arr1[3].substring(1);
-            arr1[4] = arr1[4].substring(0, 1).toUpperCase() + arr1[4].substring(1);
-            arr1[5] = arr1[5].substring(0, 1).toUpperCase() + arr1[5].substring(1);
-            threeWordSentence_1 = arr1[0] + " " + arr1[1] + " " + arr1[2];
-            System.out.println("After code : " + threeWordSentence_1);
-            return threeWordSentence_1;
+/**
+ * Write a method to reverse a String
+ */
+public static String reverseString(String userStr) {
+        String reversedStr = "";
+        for (int i=0 ; i < userStr.length() ; i++) {
+        reversedStr += userStr.charAt(userStr.length()-1-i);
+        }
+        return reversedStr;
+        }
+
+public static String reverseString2(String userStr) {
+        String reversedStr = "";
+        for (int i=userStr.length()-1 ; i >= 0 ; i--) {
+        reversedStr += userStr.charAt(i);
+        }
+        return reversedStr;
+        }
+/**
+ * Write a method which will return the result, if the given string is a Palindrome
+ *
+ * return type: boolean
+ *
+ * 1. reverse the given string
+ * 2. if reversedStr is equal to originalString
+ *      originalString is Palindrome
+ *
+ */
+public static boolean isStringPalindrome(String userStr) {
+        boolean isPalindrome = false;
+        String reversedString = reverseString(userStr);
+        return userStr.equalsIgnoreCase(reversedString);
+        }
+
+public static boolean isStringPalindrome2(String userStr) {
+        boolean isPalindrome = true;
+        for (int i=0 ; i < userStr.length()/2 ; i++) {
+        if(userStr.toLowerCase().charAt(i) != userStr.toLowerCase().charAt(userStr.length()-1-i)) {
+        isPalindrome = false;
+        break;
+        }
+        }
+        return isPalindrome;
+        }
+
+/**
+ * Write a method to return the average of a given int array
+ *
+ * Input array : {7, 10, 3, 6, 10}
+ * ans : 7.2
+ *
+ */
+public static double findArrayAverage(int[] userArray) {
+        double sum = 0;
+        for (int num : userArray) {
+        sum+=num;
+        }
+        return sum/userArray.length;
+        }
+
+/**
+ * Write a method to return the maximum value from a given int array
+ *
+ * Input array: {33, 54, 67, 19, 9, 52}
+ * max : 67
+ */
+public static int findMaxFromArray(int[] userArray) {
+        int max = userArray[0];
+        for (int i=1 ; i < userArray.length ; i++) {
+        if (max < userArray[i]) {
+        max = userArray[i];
+        }
+        }
+        return max;
         }
 
 
-        /**
-         * Write a method to reverse a String
-         */
-        public static void String reverseString (String task3String){
-            //String task3String = "happy holidays"; //Storing the backwards strings
-            String reverse = "";
-            for (int m = task3String.length() - 1; m >= 0; m--) {
-                reverse = reverse + task3String.charAt(m);
-            }
-            System.out.println(reverse);
-           return reverse;
+
+
+
+
         }
-
-        /**
-         * Write a method which will return the result, if the given string is a Palindrome
-         *
-         * return type: boolean
-         */
-        public static void String taskword (String task1Word){
-            for (int i = 0; i < task1Word.length(); i += 1) {
-                boolean isPalindrome = true;
-                int forwards = 0;
-                int backwards = task1Word.length() - 1;
-                while (forwards <= backwards) {
-                    if (task1Word.toUpperCase().charAt(forwards) != task1Word.toUpperCase().charAt(backwards)) {
-                        isPalindrome = false;
-                        break;
-                    }
-                    forwards += 1;
-                    backwards -= 1;
-                }
-                if (isPalindrome) {
-                    System.out.println(task1Word + " is a Palindrome.");
-                } else {
-                    System.out.println(task1Word + " is not a Palindrome.");
-                }
-            }
-
-            /**
-             * Write a method to return the average of a given int array
-             *
-             * Input array : {7, 10, 3, 6, 10}
-             * ans : 7.2
-             *
-             */
-            public static void int average (int[] array){
-                int sum = 0;
-
-                for (int i = 0; i < array.length; i++) {
-
-                    sum += array[i];
-
-                }
-                int avg = sum / array.length;
-                System.out.println("the average of 10 is:" + average);
-
-                System.out.println("sum of 10 is:" + sum);
-                //return 0;
-
-            }
-
-            /**
-             * Write a method to return the maximum value from a given int array
-             *
-             * Input array: {33, 54, 67, 19, 9, 52}
-             * max : 67
-             */
-            public static int getMax (int[] inputArray) {
-                int maxValue = inputArray[0];
-                for (int i = 1; i < inputArray.length; i++) {
-                    if (inputArray[i] > maxValue) {
-                        maxValue = inputArray[i];
-                    }
-                }
-                return maxValue;
-            }
-        }
-    }
-}
--- src/Homework9/Homework9.java
+++ src/Homework9/Homework9.java
@@ -1,2 +1,82 @@
-package Homework9;public class Homework9 {
-}
+package Homework9;
+import java.lang.String;
+public class Homework9 {
+    public static void main(String[] args) {
+
+
+        /**
+         * Count the words which contains "happy" in the given array.
+         *
+         * if no word contains 'happy' --> "none"
+         * if 1 word contains 'happy' --> word
+         * if 2 or more words contain 'happy' --> all the words which happy
+         *
+         * {"happy holidays" , "how are you", "have a happy vacation", "happyness is the key" , "I want happiness for you"}
+         *      {"happy holidays", "have a happy vacation", "happyness is the key"}
+         *
+         * {"happidays" , "how are you"}
+         *      none
+         *
+         * {"happy holidays" , "happiness is the key" , "I want happiness for you"}
+         *      happy holidays
+         *
+         * input: 2 ( String[] , String )
+         * return type: String[] ( {"happy holidays", null, null} )
+         *
+         *
+         */
+        boolean flag = false;
+        int index = 0;
+        int count = 0;
+        String[] yourArray = {"happy holidays" , "how are you", "have a happy vacation", "happiness is the key" , "I want happiness for you"};
+        String found = "";
+        for (int i = 0; i < yourArray.length; i++) {
+
+            if (yourArray[i].contains("happy")) {
+                flag = true;
+                index = i;
+                count++;
+
+
+            }
+        }
+        if (flag)
+            System.out.println(index);
+        else  {
+            System.out.println("not found ");
+        }
+
+        /**
+         * Find the name which has the max length.
+         *
+         * Note: If 2 or more names has same length (which is max) then send all such names
+         *
+         * {"happy, "peaceful", "loving", "dear"}
+         *      peaceful
+         *
+         * {"happy, "peace", "love", "dear"}
+         *      happy, peace
+         *
+         * input: 1 (String[])
+         * return type: String[]
+         *
+         */
+        String[] namegroupone = {"happy", "peace", "love", "dear"};
+
+       // String[] newnames = {"john", "happy", "peACe", "jOy", "LEarN", "joy", "laugh"};
+
+        String search = "peace";
+        //if (numbers.length == newnames.length){
+
+            for (int i = 0; i < namegroupone.length; i++){
+                if (namegroupone[i].length() == search.length()){
+                    System.out.println(namegroupone[i]+" WAS FOUND AT POSITION "+(i+1));
+                    System.out.println("THE WORD ASSOCIATED WITH THIS POSITION IS: "+ namegroupone[i]);
+                }
+            }
+        }
+       /* else {
+            System.out.println("ERROR: ARRAYS DO NOT HAVE THE SAME LENGTH");
+        }*/
+    }
+
